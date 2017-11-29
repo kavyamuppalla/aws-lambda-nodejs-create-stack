d3a0e128d6ccc5875b1cb5c31a220b30bdfd0e1f
[Kavya Muppalla] <[kavyamuppalla@gmail.com]>
2017-11-29 00:21:17 -0500

    Upload Sample file to S3 to invoke Tests

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ exports.handler = (event, context, callback) => {
 	AWS.config.update({ region: 'us-east-1' });
 
 	var cloudformation = new AWS.CloudFormation({region: 'us-east-1'});
+	var s3 = new AWS.S3({params: {Bucket: 'serverless-pipeline-artifacts'}, region: 'us-east-1'});
 	var params = {
 	  StackName: 'sample-node-stack',
 	  DisableRollback: false,
@@ -19,8 +20,21 @@ exports.handler = (event, context, callback) => {
 	  TimeoutInMinutes: 5
 	};
 	cloudformation.createStack(params, function(err, data) {
-	  if (err) console.log(err, err.stack); // an error occurred
-	  else     console.log(data);           // successful response
+	  if (err) {
+	  	console.log(err, err.stack); // an error occurred
+		} else {
+			console.log(data);           // successful response
+
+			//Uploading Sample file to S3
+			s3.putObject({
+	  		Bucket: 'serverless-pipeline-artifacts',
+	  		Key: 'test/sample-node.txt',
+	  		Body: 'Invoke Test'
+	  	}, function(err, data) {
+	  		console.log("Error ==== " + err);
+	  		console.log("Data ==== " + data);
+	  	});
+		}
 	});
 	
 	callback(null, "SUCCESS");
